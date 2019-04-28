369a34efcca2f4ebdbb185d19a95043b5240fda2
guilherme-caldeira <gcaldeir@gmail.com>
2019-04-28 10:09:17 -0300

    Acrescentado ponteiro de decimal

--- exercicio1.cpp
+++ exercicio1.cpp
@@ -10,13 +10,18 @@ constexpr auto RU = 2547518;
 int main()
 {
 	int inteiro = 18, *ponteiroInteiro;
-	// float decimal, *ponteiroDecimal;
+	float decimal = 75.18, *ponteiroDecimal;
 	// char caractere, * ponteiroCaractere;
 
 	printf("Valor da variavel inteiro ANTES da modificacao: %d\n", inteiro);
 	ponteiroInteiro = &inteiro; // ponteiroInteiro aponta para inteiro
 	*ponteiroInteiro = 0; // Altera o valor da variavel inteiro
-	printf("Valor da variavel inteiro APOS a modificacao: %d\n", inteiro);
+	printf("Valor da variavel inteiro APOS a modificacao: %d\n\n", inteiro);
+
+	printf("Valor da variavel decimal ANTES da modificacao: %.2f\n", decimal);
+	ponteiroDecimal = &decimal; // ponteiroDecimal aponta para decimal
+	*ponteiroDecimal = 0.53; // Altera o valor da variavel decimal
+	printf("Valor da variavel decimal APOS a modificacao: %.2f\n\n", decimal);
 
 	system("pause");
 	return 0;
